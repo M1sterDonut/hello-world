27642ada0b886233fecdb433dfc95341f2215452
M1sterDonut <lhg@tuta.io>
2017-03-16 23:54:17 +0000

    first actual .py programme
    installed Sublime Text, wrote first hello_world.py programme and named my first variable :)

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+print ("Hello Python world!")
+print ('... and hello again!')
+message = "Hello you guys"
+print (message)
+message = "Will it print the same again although I named the variable twice?"
+print (message)
