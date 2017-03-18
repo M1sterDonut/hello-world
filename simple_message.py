cae4bf3018bf3812098e3c888b2d76c70a39342a
M1sterDonut <lhg@tuta.io>
2017-03-18 23:53:36 +0000

    Add files via upload

---
+++ simple_message.py
@@ -0,0 +1,2 @@
+message_1 = "Inspired by -Dive- from Ed Sheeran..."
+print (message_1)
---
+++ simple_messages.py
@@ -0,0 +1,3 @@
+message_1 = "Don't call me baby... if you don't mean it"
+message_2 = "Don't tell me you need me... if you don't believe it"
+print (message_1, message_2)
